299586bbd6e523f81ca571faee33253603b7012f
dharamraj <dharamraj.verma@paytm.com>
2017-03-02 18:03:32 +0530

    handle normal parse and with escape

--- index.js
+++ index.js
@@ -1,61 +1,16 @@
 'use strict'
 
-var fs = require('fs');
+var csvParser = require('../lib/csvparser')
 
-var parsecsv = {
-    processCSVWithEscape: function(req, res, next) {
-        var csvfile = req.files && req.files.csvfile;
-        var e;
-        if(!csvfile || !csvfile.path){
-            e = new Error('csvfile not provided');
-            e.status = 403;
-            return next(e);
-        }
-        csvfile.delimiter = ',';
-        csvfile.header = true;
-
-        fs.readFile(csvfile.path, {encoding: 'utf-8'}, function(err, data){
-            if (!err){
-                var lines=data.replace(/\r/g, '').split('\n');
-                var result = [];
-                var start = 0;
-                var columnCount = lines[0].split(csvfile.delimiter).length;
-                var headers = [];
-                if(csvfile.header){
-                    headers=lines[0].split(',');
-                    start = 1;
-                }
-                for (var i=start; i<lines.length; i++) {
-                    var obj = {};
-                    var currentline=lines[i].split(csvfile.delimiter);
-                    if ( currentline.length === columnCount ) {
-                        if (csvfile.header){
-                            for (var j=0; j<headers.length; j++) {
-                                obj[headers[j]] = this.escape_active(currentline[j]);
-                            }
-                        } else {
-                            for (var k=0; k<currentline.length; k++) {
-                                obj[k] = this.escape_active(currentline[k]);
-                            }
-                        }
-                        result.push(obj);
-                    }
-                }
-                req.body.csvData = result;
-                next();
-            }else{
-                err.status = 400;
-                return next(err);
-            }
-        });
+var csvHandler = {
+    processCSVWithEscape: function (req, res, next) {
+        req.needToEscape = true;
+        return csvParser.parse(req, res, next);
     },
 
-    escape_active: function(x) {
-        if (x)
-            return ('' + x.replace(/"/g, '').replace(/,/g, ' ').replace(/\n/g, " ").replace(/\r/g, " ").replace(/^=/g, "'='").replace(/^\+/, "'+'").replace(/^-/, "'-'") + '');
-        else
-            return ('');
+    processCSV: function(req, res, next) {
+        return csvParser.parse(req, res, next);
     }
 }
 
-module.exports = parsecsv;
+module.exports = csvHandler;
