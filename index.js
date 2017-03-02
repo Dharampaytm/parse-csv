d5a8763cf412ce52b94d2e26d0dd9c5ded054312
dharamraj <dharamraj.verma@paytm.com>
2017-03-02 13:14:25 +0530

    process csv with escape active content

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
 'use strict'
-
 var parsecsv = {
-    escapeActiveContent: function (req, res, next) {
-        var allItems = req.body.data || req.body.csvData;
-        var errorData = [];
-        var err;
-        if (!allItems || !util.isArray(allItems)) {
-            err = new Error('Data format error');
-            err.status = 412;
-            return next(err);
+    processCSVWithEscape: function(req, res, next) {
+        var csvfile = req.files && req.files.csvfile;
+        var e;
+        if(!csvfile || !csvfile.path){
+            e = new Error('csvfile not provided');
+            e.status = 403;
+            return next(e);
         }
+        csvfile.delimiter = ',';
+        csvfile.header = true;
 
-        allItems.forEach(function (item) {
-            console.log('Item', item);
+        fs.readFile(csvfile.path, {encoding: 'utf-8'}, function(err, data){
+            if (!err){
+                var lines=data.replace(/\r/g, '').split('\n');
+                var result = [];
+                var start = 0;
+                var columnCount = lines[0].split(csvfile.delimiter).length;
+                var headers = [];
+                if(csvfile.header){
+                    headers=lines[0].split(',');
+                    start = 1;
+                }
+                for (var i=start; i<lines.length; i++) {
+                    var obj = {};
+                    var currentline=lines[i].split(csvfile.delimiter);
+                    if ( currentline.length === columnCount ) {
+                        if (csvfile.header){
+                            for (var j=0; j<headers.length; j++) {
+                                obj[headers[j]] = this.escape_active(currentline[j]);
+                            }
+                        } else {
+                            for (var k=0; k<currentline.length; k++) {
+                                obj[k] = this.escape_active(currentline[k]);
+                            }
+                        }
+                        result.push(obj);
+                    }
+                }
+                req.body.csvData = result;
+                next();
+            }else{
+                err.status = 400;
+                return next(err);
+            }
         });
+    },
 
-        // if (x)
-        //     return ('' + x.replace(/"/g, '').replace(/,/g, ' ').replace(/\n/g, " ").replace(/\r/g, " ").replace(/^=/g, "'='").replace(/^\+/, "'+'").replace(/^-/, "'-'") + '');
-        // else
-        //     return ('');
+    escape_active: function(x) {
+        if (x)
+            return ('' + x.replace(/"/g, '').replace(/,/g, ' ').replace(/\n/g, " ").replace(/\r/g, " ").replace(/^=/g, "'='").replace(/^\+/, "'+'").replace(/^-/, "'-'") + '');
+        else
+            return ('');
     }
 }
 
