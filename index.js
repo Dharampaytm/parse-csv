3660f4f642d734b202ac9e59b8feeaa9b17b9447
dharamraj <dharamraj.verma@paytm.com>
2017-03-03 12:18:44 +0530

    file as paramter and json response with data

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 'use strict'
 
-var csvParser = require('../lib/csvparser')
+var csvparser  = require('../parse-csv/lib/csvparser');
 
-var csvHandler = {
-    processCSVWithEscape: function (req, res, next) {
-        req.needToEscape = true;
-        return csvParser.parse(req, res, next);
-    },
-
-    processCSV: function(req, res, next) {
-        return csvParser.parse(req, res, next);
-    }
-}
-
-module.exports = csvHandler;
+module.exports = csvparser;
